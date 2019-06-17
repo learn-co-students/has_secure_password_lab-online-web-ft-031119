a0b1a562750a245899944779604051825369b3f0
darrendaz <darren.lou@gmail.com>
2019-06-17 10:52:19 -0700

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(name: params[:user][:name])
+    if user.try(:authenticate, params[:user][:password])
+      session[:user_id] = user.id
+      @user = user
+      redirect_to user_path(@user)
+    else
+      redirect_to new_session_path
+    end
+    
+    
+  end
+end

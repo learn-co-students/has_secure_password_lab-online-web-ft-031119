2b507c1b86364d4bd65a770326d8045573b37c4e
darrendaz <darren.lou@gmail.com>
2019-06-17 11:00:35 -0700

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       @user = user
       redirect_to user_path(@user)
     else
-      redirect_to new_session_path
+      redirect_to login_path
     end
     
     
